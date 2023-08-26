e1af49b3e2c998d531fd41b74ff016797b7fbe02
nahcg <glor.chan@mail.utoronto.ca>
2023-08-26 10:17:33 -0400

    add function and html display each photo using photoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
-};
+function PhotoListItem(props) {
+  return (
+    <div className="photo-list__item">
+      <PhotoFavButton />
+      <img className="photo-list__image" src={props.photo} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.profile} />
+        <div className="photo-list__user-info">
+          <ul className="photo-list__user-username">{props.username}</ul>
+          <ul className="photo-list__user-location">{props.city}, {props.country}</ul>
+        </div>
+      </div>
+    </div >
+  );
+}
 
 export default PhotoListItem;
