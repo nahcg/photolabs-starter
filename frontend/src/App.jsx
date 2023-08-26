58dcbb95be011ee6f7556173ebe82f7bf5ab5583
nahcg <glor.chan@mail.utoronto.ca>
2023-08-26 10:27:26 -0400

    display NavBar and PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import PhotoFavButton from 'components/PhotoFavButton';
 import './App.scss';
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <HomeRoute />
     </div>
   );
 };
