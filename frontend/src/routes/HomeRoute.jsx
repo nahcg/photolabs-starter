35c984800c81a37185b179fad8f0cd2e1e1ca227
nahcg <glor.chan@mail.utoronto.ca>
2023-08-27 11:58:15 -0400

    add state for modal visibility

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import '../styles/HomeRoute.scss';
+import TopNavigation from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
+import PhotoDetailsModal from "./PhotoDetailsModal";
+// move topicData and PhotoData to app.jsx and pass down and props
 
-const HomeRoute = () => {
+
+const HomeRoute = (props) => {
+  const { photoData, topicData } = props;
+
+  //save modal open or close as state
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState('');
+
+  const openModal = (photoId) => {
+    setSelectedPhoto(photoId);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+
+  //create favorites as empty array
+  const [favorites, setFavorites] = useState([]);
+  //toggleFavorite saves favorited photoId state in favorites array
+  const toggleFavorite = (photoId) => {
+    if (favorites.includes(photoId)) {
+      const copyOfFavorites = [...favorites].filter(favPhotoId => favPhotoId !== photoId)
+      setFavorites(copyOfFavorites)
+      return
+    }
+    setFavorites(prev => [...prev, photoId])
+  }
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation favoriteExists={favorites.length > 0} />
+      <PhotoList photoData={photoData} topicData={topicData} favorites={favorites} toggleFavorite={toggleFavorite} openModal={openModal} closeModal={closeModal} selectedPhoto={selectedPhoto} setSelectedPhoto={setSelectedPhoto} modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      {modalOpen && selectedPhoto !== null && (
+        <PhotoDetailsModal photoData={photoData} openModal={openModal} closeModal={closeModal} selectedPhoto={selectedPhoto} setSelectedPhoto={setSelectedPhoto} modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      )}
     </div>
   );
 };
+
 
 export default HomeRoute;
