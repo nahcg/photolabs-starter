c569b8e5e49d69fe373d856e0282bf9fe58b2bc6
nahcg <glor.chan@mail.utoronto.ca>
2023-09-04 16:23:22 -0400

    pass actions to HomeRoute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,44 +4,50 @@ import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 import PhotoDetailsModal from "./PhotoDetailsModal";
-// move topicData and PhotoData to app.jsx and pass down and props
 
 
 const HomeRoute = (props) => {
-  const { photoData, topicData } = props;
-
-  //save modal open or close as state
-  const [modalOpen, setModalOpen] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState('');
-
-  const openModal = (photoId) => {
-    setSelectedPhoto(photoId);
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
-
-  //create favorites as empty array
-  const [favorites, setFavorites] = useState([]);
-  //toggleFavorite saves favorited photoId state in favorites array
-  const toggleFavorite = (photoId) => {
-    if (favorites.includes(photoId)) {
-      const copyOfFavorites = [...favorites].filter(favPhotoId => favPhotoId !== photoId)
-      setFavorites(copyOfFavorites)
-      return
-    }
-    setFavorites(prev => [...prev, photoId])
-  }
+  const { 
+    photoData, 
+    topicData,
+    openModal, 
+    closeModal, 
+    toggleFavorite, 
+    setSelectedPhoto, 
+    getPhotosByTopic,
+    modalOpen,
+    selectedPhoto,
+    favorites
+  } = props;
 
   return (
     <div className="home-route">
-      <TopNavigation favoriteExists={favorites.length > 0} />
-      <PhotoList photoData={photoData} topicData={topicData} favorites={favorites} toggleFavorite={toggleFavorite} openModal={openModal} closeModal={closeModal} selectedPhoto={selectedPhoto} setSelectedPhoto={setSelectedPhoto} modalOpen={modalOpen} setModalOpen={setModalOpen} />
-      {modalOpen && selectedPhoto !== null && (
-        <PhotoDetailsModal photoData={photoData} openModal={openModal} closeModal={closeModal} selectedPhoto={selectedPhoto} setSelectedPhoto={setSelectedPhoto} modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      <TopNavigation
+        favoriteExists={favorites.length > 0}
+        topicData={topicData}
+        getPhotosByTopic={getPhotosByTopic}
+      />
+      <PhotoList
+        photoData={photoData}
+        topicData={topicData}
+        favorites={favorites}
+        toggleFavorite={toggleFavorite}
+        openModal={openModal}
+        closeModal={closeModal}
+        setSelectedPhoto={setSelectedPhoto}
+        modalOpen={modalOpen}
+        selectedPhoto={selectedPhoto}
+         />
+      {modalOpen && (selectedPhoto !== null) && (
+        <PhotoDetailsModal
+          photoData={photoData}
+          favorites={favorites}
+          toggleFavorite={toggleFavorite}
+          openModal={openModal}
+          closeModal={closeModal}
+          setSelectedPhoto={setSelectedPhoto}
+          modalOpen={modalOpen}
+          selectedPhoto={selectedPhoto} />
       )}
     </div>
   );
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,54 @@
+/* eslint-disable func-style */
 import React from 'react';
 
+import PhotoList from 'components/PhotoList';
+import PhotoFavButton from 'components/PhotoFavButton';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+function PhotoDetailsModal(props) {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={props.closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-    </div>
-  )
-};
+      <div className="photo-details-modal__fav-button">
+        <PhotoFavButton
+          photoId={props.selectedPhoto.id}
+          favorites={props.favorites}
+          toggleFavorite={props.toggleFavorite}
+        />
+      </div>
+      <div className="photo-details-modal__image-container">
+        <img src={props.selectedPhoto.urls.full} className="photo-details-modal__image" />
+        <div className="photo-details-modal____user-details">
+          <img className="photo-list__user-profile" src={props.selectedPhoto.user.profile} />
+          <div className="photo-details-modal__user-info">
+            <ul className="photo-details-modal__user-username">{props.selectedPhoto.user.name}</ul>
+            <ul className="photo-details-modal__user-location">{props.selectedPhoto.location.city}, {props.selectedPhoto.location.country}</ul>
+          </div>
+        </div>
+      </div>
+      <p className="photo-details-modal__header">
+        Similar Photos
+      </p>
+      <div className="photo-details-modal__images">
+        <PhotoList
+          photoData={Object.values(props.selectedPhoto.similar_photos)}
+          photo={props.selectedPhoto}
+          favorites={props.favorites}
+          toggleFavorite={props.toggleFavorite}
+          openModal={props.openModal}
+          closeModal={props.closeModal}
+          setSelectedPhoto={props.setSelectedPhoto}
+          modalOpen={props.modalOpen}
+          selectedPhoto={props.selectedPhoto}
+        />
+      </div>
+    </div >
+  );
+}
+
 
 export default PhotoDetailsModal;
+
